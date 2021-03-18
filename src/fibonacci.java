1eafa18eead95fb21a789c424826ee4d5b789a39
iewang117 <iewang117@outlook.com>
2021-03-18 11:12:26 +0800

    509. 斐波那契数

---
+++ src/fibonacci.java
@@ -0,0 +1,18 @@
+public class fibonacci {
+    class Solution {
+        public int fib(int n) {
+            if(n == 0){
+                return 0;
+            }
+            if(n == 1){
+                return 1;
+            }
+            int[] dp = new int[n+1];
+            dp[1] = dp[2] = 1;
+            for(int i = 2; i < n+1; i++){
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+        }
+    }
+}

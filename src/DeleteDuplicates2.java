f958973898374e51d06fea9a1d26680aa98f3bf6
iewang117 <iewang117@outlook.com>
2021-03-26 10:49:40 +0800

    83. 删除排序链表中的重复元素

---
+++ src/DeleteDuplicates2.java
@@ -0,0 +1,34 @@
+public class DeleteDuplicates2 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates2(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}

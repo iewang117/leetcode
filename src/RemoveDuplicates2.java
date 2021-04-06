9fa307bc9b979f1e343c9c9a2bb932df21298169
iewang117 <iewang117@outlook.com>
2021-04-06 10:29:31 +0800

    80. 删除有序数组中的重复项 II

---
+++ src/RemoveDuplicates2.java
@@ -0,0 +1,17 @@
+public class RemoveDuplicates2 {
+    public int removeDuplicates2(int[] nums) {
+        int len = nums.length;
+        if (len <= 2) {
+            return len;
+        }
+        int slow = 2, fast = 2;
+        while (fast < len) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}

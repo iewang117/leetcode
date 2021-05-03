d5923d814cfeb145dfbebdc9e7cf67f810474906
iewang117 <iewang117@outlook.com>
2021-05-03 13:19:39 +0800

    7. 整数反转

---
+++ src/ReverseInteger.java
@@ -0,0 +1,14 @@
+public class ReverseInteger {
+    public int reverseInteger(int x) {
+        int rev = 0;
+        while(x != 0){
+            if (rev < Integer.MIN_VALUE / 10 || rev > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x /= 10;
+        }
+        return rev;
+    }
+}

92d94daa786f24a2c519c4624a38c50bd698ecce
iewang117 <iewang117@outlook.com>
2021-05-21 10:48:27 +0800

    1035. 不相交的线

---
+++ src/MaxUncrossedLines.java
@@ -0,0 +1,18 @@
+public class MaxUncrossedLines {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        //本质上是最长公共子序列问题
+        int m = nums1.length, n = nums2.length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                //因为dp数组的0用来表示长度为0了，用i-1表示最新的一位
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}

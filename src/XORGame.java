1611056c05d3d91a9525907a7c0315b3bb2a8cef
iewang117 <iewang117@outlook.com>
2021-05-22 19:30:53 +0800

    810. 黑板异或游戏

---
+++ src/XORGame.java
@@ -0,0 +1,12 @@
+public class XORGame {
+    public boolean xorGame(int[] nums) {
+        if (nums.length % 2 == 0) {
+            return true;
+        }
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+        return xor == 0;
+    }
+}
